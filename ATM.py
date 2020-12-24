c94384d117b79346e0dd330c5812c31806c06aa2
Mustafaelhdadi <Mostafa.eng_2194@eng.kfs.edu.eg>
2020-12-24 22:22:06 +0200

    Added more features ;)

--- ATM.py
+++ ATM.py
@@ -7,26 +7,76 @@ class atm:
   def __init__(self, root):
     self.root = root
     self.root.title("ATM System")
-    self.root.geometry("774x760+280+0")
+    self.root.geometry("798x760+280+0")
     self.root.configure(bg='gainsboro')
 
-    MainFrame = Frame(self.root, bd=10, width=774, height=760, relief=RIDGE)
+    MainFrame = Frame(self.root, bd=10, width=784, height=700, relief=RIDGE)
     MainFrame.grid()
 
-    TopFrame1 = Frame(MainFrame, bd=3, width=724, height=400, relief=RIDGE)
+    TopFrame1 = Frame(MainFrame, bd=3, width=734, height=300, relief=RIDGE)
     TopFrame1.grid(row = 1, column = 0, padx = 12)
 
-    TopFrame2 = Frame(MainFrame, bd=3, width=724, height=300, relief=RIDGE)
+    TopFrame2 = Frame(MainFrame, bd=3, width=734, height=300, relief=RIDGE)
     TopFrame2.grid(row = 0, column = 0, padx = 8)
 
-    TopFrame2Left = Frame(MainFrame, bd=1, width=300, height=300, relief=RIDGE)
+    TopFrame2Left = Frame(TopFrame2, bd=1, width=190, height=300, relief=RIDGE)
     TopFrame2Left.grid(row = 0, column = 0, padx = 8)
 
-    TopFrame2Mid = Frame(MainFrame, bd=1, width=100, height=300, relief=RIDGE)
+    TopFrame2Mid = Frame(TopFrame2, bd=1, width=200, height=300, relief=RIDGE)
     TopFrame2Mid.grid(row = 0, column = 1, padx = 8)
 
-    TopFrame2Right = Frame(MainFrame, bd=1, width=300, height=300, relief=RIDGE)
+    TopFrame2Right = Frame(TopFrame2, bd=1, width=190, height=300, relief=RIDGE)
     TopFrame2Right.grid(row = 0, column = 2, padx = 8)
+    #==============================================================================
+
+    self.txtReceipt = Text(TopFrame2Mid, height=17, width=42, bd=12, font=('arial', 9, 'bold'))
+    self.txtReceipt.grid(row = 0, column = 0)
+    #==============================================================================
+
+    self.img_arrow_left = PhotoImage(file='icons/lArrow.png')
+    self.btnArrowL1 = Button(TopFrame2Left, width=160, height=60, state=DISABLED, image=self.img_arrow_left).grid(row = 0, column = 0, padx = 2, pady = 4)
+    
+    self.btnArrowL2 = Button(TopFrame2Left, width=160, height=60, state=DISABLED, image=self.img_arrow_left).grid(row = 1, column = 0, padx = 2, pady = 4)
+    
+    self.btnArrowL3 = Button(TopFrame2Left, width=160, height=60, state=DISABLED, image=self.img_arrow_left).grid(row = 2, column = 0, padx = 2, pady = 4)
+    
+    self.btnArrowL4 = Button(TopFrame2Left, width=160, height=60, state=DISABLED, image=self.img_arrow_left).grid(row = 3, column = 0, padx = 2, pady = 4)
+    #==============================================================================
+
+    self.img_arrow_right = PhotoImage(file='icons/rArrow.png')
+    self.btnArrowR1 = Button(TopFrame2Right, width=160, height=60, state=DISABLED, image=self.img_arrow_right).grid(row = 0, column = 0, padx = 2, pady = 4)
+    
+    self.btnArrowR2 = Button(TopFrame2Right, width=160, height=60, state=DISABLED, image=self.img_arrow_right).grid(row = 1, column = 0, padx = 2, pady = 4)
+    
+    self.btnArrowR3 = Button(TopFrame2Right, width=160, height=60, state=DISABLED, image=self.img_arrow_right).grid(row = 2, column = 0, padx = 2, pady = 4)
+    
+    self.btnArrowR4 = Button(TopFrame2Right, width=160, height=60, state=DISABLED, image=self.img_arrow_right).grid(row = 3, column = 0, padx = 2, pady = 4)
+    #==============================================================================
+
+    self.img1 = PhotoImage(file='icons/one.png')
+    self.btn1 = Button(TopFrame1, width=160, height=60, image=self.img1).grid(row = 2, column = 0, padx = 2, pady = 4)
+    
+    self.img2 = PhotoImage(file='icons/two.png')
+    self.btn2 = Button(TopFrame1, width=160, height=60, image=self.img2).grid(row = 2, column = 1, padx = 2, pady = 4)
+    
+    self.img3 = PhotoImage(file='icons/three.png')
+    self.btn3 = Button(TopFrame1, width=160, height=60, image=self.img3).grid(row = 2, column = 2, padx = 2, pady = 4)
+    
+    self.imgCE = PhotoImage(file='icons/cancel.png')
+    self.btn3 = Button(TopFrame1, width=160, height=60, image=self.imgCE).grid(row = 2, column = 3, padx = 2, pady = 4)
+    
+    self.img4 = PhotoImage(file='icons/four.png')
+    self.btn4 = Button(TopFrame1, width=160, height=60, image=self.img4).grid(row = 3, column = 0, padx = 2, pady = 4)
+
+    self.img5 = PhotoImage(file='icons/five.png')
+    self.btn5 = Button(TopFrame1, width=160, height=60, image=self.img5).grid(row = 3, column = 1, padx = 2, pady = 4)
+    
+    self.img6 = PhotoImage(file='icons/six.png')
+    self.btn6 = Button(TopFrame1, width=160, height=60, image=self.img6).grid(row = 3, column = 2, padx = 2, pady = 4)
+    
+    self.imgCL = PhotoImage(file='icons/clear.png')
+    self.btnCL = Button(TopFrame1, width=160, height=60, image=self.imgCL).grid(row = 3, column = 3, padx = 2, pady = 4)
+
 
 if __name__=='__main__':
   root = Tk()
